5698c982f6905eeb890e220391927fc3549c705d
vishwa jayasinghe <103561819+vishwa2001911@users.noreply.github.com>
2024-01-24 19:11:50 +0530

    update

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar.jsx";
+
+
+function Home(){
+
+    return(
+        <>
+        <Navbar />
+        <h1>hello</h1>
+        </>
+    )
+}
+
+export default Home;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar(){
+
+
+    return(
+        <>
+        <div className="nav-container">
+            <h1>Nav</h1>
+        </div>
+        </>
+    )
+}
+
+
+export default Navbar;
